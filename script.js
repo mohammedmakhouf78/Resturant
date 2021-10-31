a18757e0c92cebbcad9c7ddf654478d8382d96a1
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2021-10-31 22:25:02 +0200

    header finished

--- script.js
+++ script.js
@@ -19,3 +19,45 @@ blackBg.addEventListener("click", function () {
   ul.classList.toggle("active");
   blackBg.classList.toggle("black-bg-active");
 });
+
+// Carousel
+
+let next = document.querySelector(".carousel .next");
+
+let prev = document.querySelector(".carousel .prev");
+
+let slides = document.querySelectorAll(".carousel>div");
+
+let index = 1;
+
+next.addEventListener("click", function () {
+  if (index == 1) {
+    slides[0].style.transform = "translateX(-100%)";
+    slides[1].style.transform = "translateX(0%)";
+    index = 2;
+  }
+  if (index == 2) {
+  }
+  if (index == 0) {
+    slides[2].style.transform = "translateX(-100%)";
+    slides[0].style.transform = "translateX(0%)";
+    index = 1;
+  }
+});
+
+prev.addEventListener("click", function () {
+  if (index == 1) {
+    slides[0].style.transform = "translateX(100%)";
+    slides[2].style.transform = "translateX(0%)";
+    index = 0;
+  }
+
+  if (index == 0) {
+  }
+
+  if (index == 2) {
+    slides[1].style.transform = "translateX(100%)";
+    slides[0].style.transform = "translateX(0%)";
+    index = 1;
+  }
+});
