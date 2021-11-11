dd1555c49157fdbd4f74ff712f53af18f1f487c7
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2021-11-11 19:53:32 +0200

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -139,3 +139,41 @@ var swiper = new Swiper(".swiper-container", {
     // },
   },
 });
+
+var swiper = new Swiper(".swiper-container2", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
+  },
+  // init: false,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".prev-btn",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 0.5,
+      spaceBetween: 20,
+    },
+
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    // 1440:{
+    //   slidesPerView: 5,
+    //   spaceBetween: 10,
+    // },
+  },
+});
